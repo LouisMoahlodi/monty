b7511566a4060c0f422b8f99c372087f88149a89
LouisMoahlodi <110418827+LouisMoahlodi@users.noreply.github.com>
2023-06-05 22:18:37 +0200

    Added the pall file

---
+++ pall.c
@@ -0,0 +1,31 @@
+#include "monty.h"
+
+/**
+ * f_pall - pritns the stack
+ * 
+ * @head: stack head
+ * @counter: no used
+ * 
+ * Return: void
+ */
+void f_pall(stack_t **head, unsigned int counter)
+{
+    stack_t *h;
+
+    /* Suppress unused variable warning*/
+    (void)counter;
+
+    /* Assign the stack head to a local variable*/
+    h = *head;
+
+    /* If the stack is empty, return early*/
+    if (h == NULL)
+        return;
+
+        /* Traverse the stack and print each element*/
+        while (h)
+        {
+            printf("%d\n", h->n);
+            h = h->next;
+        }
+}
