2d306dfbebd3e9d64f7e77b47cf9ab6188212d04
LouisMoahlodi <110418827+LouisMoahlodi@users.noreply.github.com>
2023-06-14 22:37:14 +0200

    change execute file

--- execute.c
+++ execute.c
@@ -11,7 +11,7 @@ int execute(char *content, stack_t **stack, unsigned int counter, FILE *file)
 	instruction_t opst[] = {
 		{"push", f_push}, {"pall", f_pall}, {"pint", f_pint},
 		{"pop", f_pop},
-		{"swap", f_swap},
+		/*{"swap", f_swap},
 		{"add", f_add},
 		{"nop", f_nop},
 		{"sub", f_sub},
@@ -21,9 +21,9 @@ int execute(char *content, stack_t **stack, unsigned int counter, FILE *file)
 		{"pchar", f_pchar},
 		{"pstr", f_pstr},
 		{"rotl", f_rotl},
-		{"rotr", f_rotr},
+		{"rotr", f_rotr},*/
 		{"queue", f_queue},
-		{"stack", f_stack},
+		/*{"stack", f_stack},*/
 		{NULL, NULL}
 	};
 	unsigned int i = 0;
