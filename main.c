49f504e4f0816573ead87c28da61c66b52b51f44
LouisMoahlodi <110418827+LouisMoahlodi@users.noreply.github.com>
2023-06-14 21:27:47 +0200

    Edit the header file

--- main.c
+++ main.c
@@ -1,9 +1,15 @@
 #include "monty.h"
 #include <stdio.h>
 #include <stdlib.h>
-
 bus_t bus = {NULL, NULL, NULL, 0};
-
+/**
+ * main - monty code interpreter
+ * 
+ * @argc: number of arguments 
+ * @argv: monty file location 
+ * 
+ * Return: 0 on success 
+ */
 int main(int argc, char *argv[])
 {
     char *content;
